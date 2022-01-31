022d3d780aee9340532c9ef40f7ffa109a5ce2a3
Andre gois <andre.s.gois3@gmail.com>
2022-01-31 20:23:17 -0300

    refactor: splitting files

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,6 @@
-import express from "express";
-import path from "path";
-import { createServer } from "http";
-import { Server } from "socket.io";
+import { server } from "./http";
+import "./websocket/ChatService";
 
-const app = express();
-
-const server = createServer(app);
-
-app.use(express.static(path.join(__dirname, "..","public")));
-
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-  console.log("Socket: ", socket.id);
-})
-
-app.get("/", (req, res) => {
-  return res.json({
-    message: "Hello WebSocket"
-  })
-})
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
