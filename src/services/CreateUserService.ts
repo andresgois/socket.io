9d078f86f4ea8dd7981cf596963fb6cdd636d2e6
Andre gois <andre.s.gois3@gmail.com>
2022-01-31 21:24:19 -0300

    feat: class to create user

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import { injectable } from "tsyringe";
+import { User } from "../schema/User";
+
+interface CreateUserDTO {
+  email: string;
+  socket_id: string;
+  name: string;
+  avatar: string;
+}
+
+
+@injectable()
+class CreateUserService {
+
+  async execute({ email, socket_id, avatar, name }: CreateUserDTO){
+    const userAlreadyExists = await User.findOne({
+      email,
+    }).exec();
+
+    if(userAlreadyExists) {
+      const user = await User.findOneAndUpdate(
+        {
+          _id: userAlreadyExists._id,
+        },
+        {
+          $set: { socket_id, avatar, name },
+        }
+      );
+      return user;
+    } else {
+      const user = await User.create({
+        email,
+        socket_id,
+        name,
+        avatar,
+      });
+      return user;
+    }
+  }
+}
+
+export { CreateUserService }
