02e729cfb233b714b5797b81212a4688fe8188c1
Andre gois <andre.s.gois3@gmail.com>
2022-01-31 21:23:15 -0300

    feat: connect mongo

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,19 @@
+import "reflect-metadata";
 import express from "express";
 import path from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import mongoose, { ConnectOptions } from "mongoose";
 
 const app = express();
 
 const server = createServer(app);
+
+
+mongoose.connect("mongodb://localhost/rocketsoket", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions)
 
 app.use(express.static(path.join(__dirname, "..","public")));
 
