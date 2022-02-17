0985c1dce399e263b9642a08e789c7157f5db5d8
Andre gois <andre.s.gois3@gmail.com>
2022-02-17 20:14:19 -0300

    feat: add listening methods

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,9 +1,39 @@
+import { container } from "tsyringe";
 import { io } from "../http";
+import { CreateUserService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
 
 io.on("connect", (socket) => {
-  // io.emit(); // Envia para todos, global
-  //socket.emit(); // individual
-  socket.emit("chat_iniciado", {
-    message: "Seu chat foi inciado",
+  
+  socket.on("start", async (data) => {
+    const { email, avatar, name } = data;
+    const createUserService = container.resolve(CreateUserService);
+    console.log('Data ',data)
+    
+    const user = await createUserService.execute({
+      email,
+      avatar,
+      name,
+      socket_id: socket.id
+    })
+
+    console.log(user);
+
+    socket.broadcast.emit("new_users", user);
+
   });
+
+  socket.on("get_users", async (callback) => {
+    const getAllUsersService = container.resolve(GetAllUsersService);
+    const users = await getAllUsersService.execute();
+
+    callback(users);
+  })
 });
+
+
+    // io.emit(); // Envia para todos, global
+    //socket.emit(); // individual
+    /*socket.emit("chat_iniciado", {
+      message: "Seu chat foi inciado",
+    });*/
