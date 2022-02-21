7bf72e8ba09cc40dad3f33372b84efd4bbca8145
Andre gois <andre.s.gois3@gmail.com>
2022-02-21 20:58:17 -0300

    feat: find the room id

---
+++ src/services/GetChatRoomByIdService.ts
@@ -0,0 +1,19 @@
+import { injectable } from "tsyringe";
+import { ChatRoom } from "../schema/ChatRoom";
+
+
+@injectable()
+class GetChatRoomByIdService {
+
+  async execute(idChatRoom: string) {
+    const room = await ChatRoom.findOne({
+      idChatRoom, 
+    })
+    .populate("idUsers")
+    .exec();
+
+    return room;
+  }
+}
+
+export { GetChatRoomByIdService }
